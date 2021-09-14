2b1a8bab7f2b1897d5a2b965f0bbacecdf54a951
Nejc Drobnič <ethoarx@gmail.com>
2021-09-14 05:38:36 +0200

    feat: static data endpoint

--- rarity_tools_scraper_api/services/projects.py
+++ rarity_tools_scraper_api/services/projects.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Path
 from fastapi_cache.decorator import cache
-from rarity_tools_scraper.projects import Config, get_collection_config_static
+from rarity_tools_scraper.projects import Config, get_collection_config_static, get_collection_staticdata_static, \
+	StaticData
 
 router = APIRouter(
 	prefix="/projects",
@@ -17,3 +18,14 @@ async def config_collection(
 		collection_id: str = Path(default="", description="ID of the desired collection")
 ) -> Config:
 	return get_collection_config_static(collection_id)
+
+
+@router.get(
+	"/staticdata/{collection_id}",
+	response_model=StaticData
+)
+@cache(expire=60 * 60)
+async def staticdata_collection(
+		collection_id: str = Path(default="", description="ID of the desired collection")
+) -> StaticData:
+	return get_collection_staticdata_static(collection_id)
