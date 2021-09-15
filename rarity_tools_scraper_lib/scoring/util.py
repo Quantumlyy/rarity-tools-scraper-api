003548e13c8e48a5e6b6d9034e3bc6cbfb69d14a
Nejc Drobnič <ethoarx@gmail.com>
2021-09-15 05:41:15 +0200

    refactor: start using dynamic config

--- rarity_tools_scraper_lib/scoring/util.py
+++ rarity_tools_scraper_lib/scoring/util.py
@@ -1,9 +1,38 @@
-from rarity_tools_scraper.projects import BasePropDef
+from rarity_tools_scraper.projects import BasePropDef, ConfigRankingsPreset
 
-# "options" would come from config.py
+
 # key: "getPropWeight",
 #   value: function(t, e, r) {
-#     return t.isMatch && !r || e.combined && t.hasCombined || !e.combined && t.isCombined ? 0 : e.propWeights && void 0 !== e.propWeights[t.name] && e.weights && "data" == t.type && "score" == e.method ? e.propWeights[t.name] : "primaryKey" == t.type || "data" == t.type ? 0 : e.weights && e.propWeights ? void 0 === e.propWeights[t.name] ? 1 : e.propWeights[t.name] : 1
+#     return t.isMatch && !r || e.combined && t.hasCombined || !e.combined && t.isCombined
+#       ? 0
+#       : e.propWeights && void 0 !== e.propWeights[t.name] && e.weights && "data" == t.type && "score" == e.method
+#         ? e.propWeights[t.name]
+#         : "primaryKey" == t.type || "data" == t.type
+#           ? 0
+#           : e.weights && e.propWeights
+#             ? void 0 === e.propWeights[t.name]
+#               ? 1
+#               : e.propWeights[t.name]
+#             : 1
 #   }
-def get_prop_weight(base_prop_def: BasePropDef, options, matches: str):
-	base_prop_def["isMatch"] and not matches or options["combined"] and t["isCombined"]
+def get_prop_weight(base_prop_def: BasePropDef, options: ConfigRankingsPreset, matches: str):
+	prop_defs_weight = options.prop_weights[base_prop_def.name]
+	# TODO: has_combined
+	if base_prop_def.is_match and not matches or options.combined and base_prop_def["has_combined"] \
+		or not options.combined and base_prop_def.is_combined:
+		return 0
+	else:
+		if options.prop_weights and prop_defs_weight is not None and options.weights and base_prop_def.type == "data" \
+				and options.method == "score":
+			return prop_defs_weight
+		else:
+			if base_prop_def.type == "primaryKey" and options.method == "data":
+				return 0
+			else:
+				if options.weights and options.prop_weights:
+					if prop_defs_weight is None:
+						return 0
+					else:
+						return prop_defs_weight
+				else:
+					return 1
